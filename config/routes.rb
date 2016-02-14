b2d339155a03770b7fa4aff05a155a6072b1d7fd
takao.horiguchi <horiguchi.t@opst.co.jp>
2016-02-14 19:51:47 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  get 'items/new' => 'items#new'
-  get 'items/:id' => 'items#show', as:  :item
-  patch 'items/:id' => 'items#update'
-  delete 'items/:id' => 'items#destroy'
-  get 'items/:id/edit' => 'items#edit'
-  get 'items' => 'items#index'
-  post 'items' => 'items#create'
+  resources :items
+
+  # localhost:3000だけでapplicationの画面に飛ぶようにする
+  root 'items#index'
+  # get 'items/new' => 'items#new'
+  # get 'items/:id' => 'items#show', as:  :item
+  # patch 'items/:id' => 'items#update'
+  # delete 'items/:id' => 'items#destroy'
+  # get 'items/:id/edit' => 'items#edit'
+  # get 'items' => 'items#index'
+  # post 'items' => 'items#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
